607497ac6958e817b9b7207e9efd9a7fe7c176a1
Jesper <jesper@acorneroftheweb.com>
2021-06-08 23:12:24 +0200

    Map moves along with character

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use bevy::{
     sprite::collide_aabb::{collide, Collision},
 };
 const TIME_STEP: f32 = 1.0 / 60.0;
+pub struct Position(Transform);
 
 fn main() {
     let window = WindowDescriptor {
@@ -16,8 +17,6 @@ fn main() {
         ..Default::default()
     };
 
-
-
     App::build()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
@@ -26,7 +25,8 @@ fn main() {
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
-                .with_system(movement_person.system()),
+                .with_system(movement_person.system())
+                .with_system(move_camera.system()),
                 )
         .run();
 }
@@ -45,8 +45,13 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    commands
+        .spawn()
+        .insert_bundle(OrthographicCameraBundle::new_2d())
+        .insert(Position(Transform::from_translation(Vec3::new(
+            0.0, 0.0, 1000.0,
+        ))));
 
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     let asset_handler = materials.add(asset_server.load("Overworld.png").into());
     commands.spawn().insert_bundle(SpriteBundle {
         material: asset_handler.clone(),
@@ -66,11 +71,14 @@ fn movement_person(
     keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<(&mut Person, &mut Transform)>,
 ) {
-    if let Ok((mut Person, mut transform)) = query.single_mut() {
+    if let Ok((mut _person, mut transform)) = query.single_mut() {
         let mut vel = Vec3::new(0.0,0.0,0.0);
-        Person.speed = 50.0;
+        let speed;
         if keyboard_input.pressed(KeyCode::LShift){
-            Person.speed = 100.0;
+            speed = 100.0;
+        }
+        else{
+            speed = 50.0;
         }
 
         if keyboard_input.pressed(KeyCode::W) {
@@ -87,9 +95,40 @@ fn movement_person(
         }
 
         let translation = &mut transform.translation;
-        translation.x += vel.x * Person.speed * TIME_STEP;
-        translation.y += vel.y * Person.speed * TIME_STEP;
-        
+        translation.x += vel.x * speed * TIME_STEP;
+        translation.y += vel.y * speed * TIME_STEP;
     }
+}
 
+fn move_camera(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut Position)>,
+) {
+    for (mut transform, mut _position) in query.iter_mut() {
+        let mut vel = Vec3::new(0.0,0.0,0.0);
+        let speed;
+        if keyboard_input.pressed(KeyCode::LShift){
+            speed = 100.0;
+        }
+        else{
+            speed = 50.0;
+        }
+
+        if keyboard_input.pressed(KeyCode::W) {
+            vel.y = 1.0;
+        }
+        else if keyboard_input.pressed(KeyCode::S) {
+            vel.y = -1.0;
+        }
+        if keyboard_input.pressed(KeyCode::A) {
+            vel.x = -1.0;
+        }
+        else if keyboard_input.pressed(KeyCode::D) {
+            vel.x = 1.0;
+        }
+
+        let translation = &mut transform.translation;
+        translation.x += vel.x * speed * TIME_STEP;
+        translation.y += vel.y * speed * TIME_STEP;
+    }
 }
