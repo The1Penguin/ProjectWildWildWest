0ca5bdbca1a9df0a790e60a60cc854a42437e736
Jesper <jesper@acorneroftheweb.com>
2021-06-16 12:24:16 +0200

    Added comments to code

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -15,6 +15,7 @@ impl Plugin for GamePlugin {
 const TIME_STEP: f32 = 1.0 / 60.0;
 pub struct Position(Transform);
 
+// The different states the game can be in
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum AppState{
     Load,
@@ -24,7 +25,7 @@ pub enum AppState{
     Menu,
 }
 
-
+// Struct to indicate different characters
 struct Person{
     speed: f32,
     mc: bool,
--- src/game/overworld.rs
+++ src/game/overworld.rs
@@ -28,18 +28,20 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    // Spawns in the camera
     commands
         .spawn()
         .insert_bundle(OrthographicCameraBundle::new_2d())
         .insert(Position(Transform::from_translation(Vec3::new(
             0.0, 0.0, 1000.0,
         ))));
-
+    // Spawns in the overworld image
     let asset_handler = materials.add(asset_server.load("Overworld.png").into());
     commands.spawn().insert_bundle(SpriteBundle {
         material: asset_handler.clone(),
         ..Default::default()
     });
+    // Spawns in wes
     let sprite_handler = materials.add(asset_server.load("wes.png").into());
     commands.spawn().insert_bundle(SpriteBundle {
         material: sprite_handler.clone(),
@@ -54,15 +56,19 @@ fn movement<'a>(
     transform: &mut Transform,
     init_speed: f32
 ) -> Res<'a, Input<KeyCode>> {
+    // Direction and speed init
     let mut vel = Vec3::new(0.0,0.0,0.0);
     let speed;
+
+    // If speedup is held, double the inital speed
     if keyboard_input.pressed(KeyCode::LShift){
         speed = 2.0 * init_speed;
     }
     else{
         speed = init_speed;
     }
-
+    
+    // Move in the specified direction
     if keyboard_input.pressed(KeyCode::W) {
         vel.y = 1.0;
     }
@@ -76,6 +82,7 @@ fn movement<'a>(
         vel.x = 1.0;
     }
 
+    // Apply the changes
     let translation = &mut transform.translation;
     translation.x += vel.x * speed * TIME_STEP;
     translation.y += vel.y * speed * TIME_STEP;
@@ -86,6 +93,7 @@ fn movement_person(
     keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<(&mut Person, &mut Transform)>,
 ) {
+    // Finds the person to move and moves them
     if let Ok((mut _person, mut transform)) = query.single_mut() {
         movement(keyboard_input, &mut transform, 50.0);
     }
@@ -95,6 +103,7 @@ fn move_camera(
     mut keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<(&mut Transform, &mut Position)>,
 ) {
+    // Moves all cameras
     for (mut transform, mut _position) in query.iter_mut() {
         keyboard_input = movement(keyboard_input, &mut transform, 50.0);
     }
