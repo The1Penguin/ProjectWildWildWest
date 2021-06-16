1e1d9f1c409f2cfb6c9152d96bf2b1d8814d3b51
Jesper <jesper@acorneroftheweb.com>
2021-06-16 12:29:06 +0200

    Made the game launch in borderless fullscreen with correct title

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::{
     core::FixedTimestep,
     prelude::*,
+    window::WindowMode,
     render::pass::ClearColor,
     sprite::collide_aabb::{collide, Collision},
 };
@@ -15,13 +16,15 @@ fn main() {
         height: 1080.0,
         vsync: true,
         resizable: false,
+        mode: bevy::window::WindowMode::BorderlessFullscreen,
         ..Default::default()
     };
 
     App::build()
+        .insert_resource(ClearColor(Color::BLACK))
+        .insert_resource(window)
         .add_plugins(DefaultPlugins)
         .add_system(bevy::input::system::exit_on_esc_system.system())
-        .insert_resource(window)
         .add_plugin(GamePlugin)
         .run();
 }
